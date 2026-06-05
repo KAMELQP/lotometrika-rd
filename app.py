ca923e056c157e20043f68294716b8a3e6e433c5
KAMELQP <developkml@gmail.com>
2026-06-05 13:42:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
- import streamlit as st
 
-st.title("Lotometrika RD")
-st.write("Aplicación funcionando correctamente")
+import streamlit as st
+import pandas as pd
+
+st.title("LOTOMETRIKA RD")
+
+df = pd.read_csv("historicos.csv")
+
+st.subheader("Histórico cargado")
+st.dataframe(df)
+
+st.write("Cantidad de sorteos:", len(df))
+
+frecuencia = pd.concat([
+    df["primero"],
+    df["segundo"],
+    df["tercero"]
+]).value_counts()
+
+st.subheader("Top 20 números más frecuentes")
+st.dataframe(frecuencia.head(20))
